c7452eb75186f2d0f1fc61f90cd1bb7fb5dcafcf
robquintero <robquinterobiz@gmail.com>
2024-11-20 16:03:09 -0500

    Update Dashboard.tsx

--- src/sections/Dashboard/Dashboard.tsx
+++ src/sections/Dashboard/Dashboard.tsx
@@ -46,7 +46,7 @@ export default function Dashboard() {
   return (
     <>
       <WelcomeBanner />
-      <h2 style={{ textAlign: 'center' }}>Games</h2>
+      <h2 style={{ textAlign: 'center' }}>Exclusive Games</h2>
       <GameGrid />
     </>
   )
