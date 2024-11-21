0f440c7ad0566d1908d994678fb5d05405b3d5e8
robquintero <robquinterobiz@gmail.com>
2024-11-21 16:38:29 -0500

    Update constants.ts

--- src/games/Mines/constants.ts
+++ src/games/Mines/constants.ts
@@ -6,4 +6,4 @@ export { default as SOUND_EXPLODE } from './explode.mp3'
 
 export const GRID_SIZE = 16
 export const PITCH_INCREASE_FACTOR = 1.06
-export const MINE_SELECT = [1, 3, 5, 10, 15]
+export const MINE_SELECT = [1, 2, 3, 4, 5, 10, 15]
